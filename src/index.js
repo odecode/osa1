f4f5d0d10a969f92e8d9426847a0c141f098b306
Otto Westerlund <wotto92@hotmail.com>
2020-04-15 13:24:56 +0300

    osa 1.2 versio

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) =>{
+  return(
+    <h1>{props.text}</h1>
+  )
+}
+
+const Part = (props) =>{
+  return(
+    <p>{props.part} {props.ex}</p>
+  )
+}
+
+const Content = (props) =>{  
+  return(
+    <div>
+      <Part part = {props.p1} ex = {props.ex1} />
+      <Part part = {props.p2} ex = {props.ex2} />
+      <Part part = {props.p3} ex = {props.ex3} />
+    </div>
+  )
+}
+
+const Total = (props) =>{
+  return(
+   <p>Number of exercises {props.sum}</p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header text={course}/>
+      <Content 
+      p1={part1} ex1={exercises1}
+      p2={part2} ex2={exercises2}
+      p3={part3} ex3={exercises3}
+       />
+      <Total sum={exercises1+exercises2+exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
