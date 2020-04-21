341a2229b339368a4e557e14846aec0a43202ebf
odecode <wotto92@hotmail.com>
2020-04-21 22:32:11 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,55 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) =>{
+const Button = (props) => {
   return(
-    <h1>{props.text}</h1>
+  <button onClick={props.handler}>{props.text}</button>
   )
 }
 
-const Part = (props) =>{
-  return(
-    <p>{props.part} {props.ex}</p>
-  )
-}
-
-const Content = (props) =>{  
-  return(
-    <div>
-      <Part part = {props.p1} ex = {props.ex1} />
-      <Part part = {props.p2} ex = {props.ex2} />
-      <Part part = {props.p3} ex = {props.ex3} />
-    </div>
-  )
-}
-
-const Total = (props) =>{
-  return(
-   <p>Number of exercises {props.sum}</p>
-  )
-}
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Uint8Array(6))
+  const [max, setMax] = useState(0)
+ 
 
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const nextAnecdote = () => {
+    return(
+      setSelected(Math.floor(Math.random() * 6))
+    )
+  }
+
+  const handleVoteButton = () =>{
+    const copy = [...votes]
+    copy[selected] += 1
+    let maxCopy = Math.max(...copy)
+    let index = 0
+    for(let i = 0; i < copy.length; i++){
+      if(copy[i] >= maxCopy) index = i
+    }
+    return (setVotes(copy), setMax(index))
+  }
+  
+ 
 
   return (
+    <>
     <div>
-      <Header text={course}/>
-      <Content 
-      p1={part1} ex1={exercises1}
-      p2={part2} ex2={exercises2}
-      p3={part3} ex3={exercises3}
-       />
-      <Total sum={exercises1+exercises2+exercises3} />
+      <h1>Anecdote of the day</h1>  
+      {props.anecdotes[selected]} 
+      <br></br>
+      has {votes[selected]} votes
+      <br></br>
+      <Button text='next anecdote' handler={nextAnecdote} /> <Button text='vote' handler={handleVoteButton} />
     </div>
+    <div>
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[max]}
+    </div>
+    </>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
